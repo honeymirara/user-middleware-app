bee8eb42349f6604ef65b0ec10644afb4c2b107f
honeymirara <honeymirara@gmail.com>
2023-04-27 12:34:18 -1100

    add-request

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res){
+app.get('/', (req, res) => {
     res.send('ok')
 });
 
 app.use((err, req, res, next) => {
-
+    res.send(err.message);
 })
 
 module.exports = app;
