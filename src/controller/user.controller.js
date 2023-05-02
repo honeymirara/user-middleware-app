d842432587266ee2f86704071a4032b120d8ee16
honeymirara <honeymirara@gmail.com>
2023-05-01 13:07:34 -1100

    get-allById

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const getAllUsers = require('../service/user.service')
+const {getAllUsers} = require('../service/user.service');
 
 const route = express.Router();
 
 route.get('/', (req, res) => {
     const data = getAllUsers();
-    res.send('hi');
+    res.send(data);
 });
 
 module.exports = route;
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -5,6 +5,7 @@ const path = ('./storage/storage.json');
 
 function getAllUsers() {
     const array = JSON.parse(fs.readFileSync(path));
+    return array
 }
 
 module.exports ={getAllUsers};
